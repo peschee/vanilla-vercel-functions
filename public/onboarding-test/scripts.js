c77e9db1e1167eed8de139da6be186efde327d6a
Peter Siska <63866+peschee@users.noreply.github.com>
2024-06-25 13:49:02 +0200

    Add XHR test

---
+++ public/onboarding-test/scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const output = document.querySelector('[data-output]');
+
+    document.querySelector('[data-headers]').addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        if (output) {
+            output.innerHTML = '';
+        }
+
+        const { headers: url } = e.target.dataset;
+        const response = await fetch(url);
+        const headers = await response.json();
+
+        console.log(headers);
+        if (output) {
+            output.innerHTML = JSON.stringify(headers, null, 2)
+        }
+    })
+})
