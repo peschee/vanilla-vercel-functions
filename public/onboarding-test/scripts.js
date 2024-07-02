3956f31fa07d93f1eb498b011980eb9dad54230c
Peter Siska <63866+peschee@users.noreply.github.com>
2024-07-02 18:19:16 +0200

    Add mutation example

--- public/onboarding-test/scripts.js
+++ public/onboarding-test/scripts.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    document.querySelector('[data-graphql]').addEventListener('click', async (e) => {
+    document.querySelector('[data-graphql-query]').addEventListener('click', async (e) => {
         e.preventDefault();
 
         const tokenElement = document.querySelector('[data-token]');
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
             output.innerHTML = '';
         }
 
-        const { graphql: url } = e.target.dataset;
+        const { graphqlQuery: url } = e.target.dataset;
 
         const response = await fetch(url, {
             method: 'POST',
@@ -44,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Authorization': `Bearer ${ token }`,
             },
             body: JSON.stringify({
-                operationName: 'AggregateOneList',
                 query: `query AggregateOneList { aggregateone: AggregateOneView {id name } }`,
                 variables: {},
             }),
@@ -54,6 +53,62 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(aggregates);
         if (output) {
             output.innerHTML = JSON.stringify(aggregates, null, 2);
+        }
+    });
+
+    document.querySelector('[data-graphql-mutation]').addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        const tokenElement = document.querySelector('[data-token]');
+        const token = tokenElement ? tokenElement.value : null;
+        if (!token) {
+            console.warn('No token found');
+            return;
+        }
+
+        if (output) {
+            output.innerHTML = '';
+        }
+
+        const { graphqlMutation: url } = e.target.dataset;
+
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'omit',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${ token }`,
+            },
+            body: JSON.stringify({
+                query: `
+                    mutation createProperty($type: String) {
+                        createProperty(
+                            type: $type, 
+                            prefilled: false, 
+                            rooms: 1.5, 
+                            livingSpace: 2, 
+                            constructionYear: 2024, 
+                            address: { 
+                                street: "Technoparkstrasse", 
+                                streetNumber: "1", 
+                                zip: "8005", 
+                                city: "Zürich", 
+                                country: "Switzerland"
+                            }
+                        ) {
+                            id
+                        }
+                    }`,
+                variables: {
+                    type: 'home',
+                },
+            }),
+        });
+        const id = await response.json();
+
+        console.log(id);
+        if (output) {
+            output.innerHTML = JSON.stringify(id, null, 2);
         }
     });
 });
